e8e60da03910e84b7d53cb8ac9a6c5e83d87dfa8
horpto <__singleton__@hackerdom.ru>
2016-05-01 18:24:02 +0500

    not parse querystring of uri twice

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var url = require('url')
+var querystring = require('querystring');
 var assert = require('assert')
 
 module.exports = parse
@@ -7,13 +8,16 @@ parse.parse = parse
 
 function parse (str, parseQueryString) {
   assert.equal(typeof str, 'string')
-  var p = url.parse(str, parseQueryString)
-  if (!p.slashes)
-    p = url.parse('http://' + str, parseQueryString)
-  else if (!p.protocol)
-    p = url.parse('http:' + str, parseQueryString)
-
-  return p
+  var p = url.parse(str, false);
+  if (!p.slashes) {
+    p = url.parse('http://' + str, false);
+  } else if (!p.protocol) {
+    p = url.parse('http:' + str, false);
+  }
+  if (parseQueryString) {
+    p.query = querystring.parse(p.query);
+  }
+  return p;
 }
 
 function format (str) {
